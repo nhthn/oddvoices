8b2c17c8c7fe8fc63c58df88a4e09978607c1ee6
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-02-12 17:55:12 -0800

    Fix errors in word list

--- phonology.py
+++ phonology.py
@@ -50,13 +50,13 @@ DIPHTHONGS = [
 
 VOWELS = [
     "{}",
+    "@`",
     "A",
     "I",
     "E",
     "@",
     "u",
     "U",
-    "@`",
     "i",
 ] + DIPHTHONGS
 
@@ -120,89 +120,13 @@ def normalize_pronunciation(pronunciation):
         pronunciation = pronunciation + ["_"]
     return pronunciation
 
-def generate_nonsense_word_list():
-    word_list = []
-    for i, phoneme_1 in enumerate(ALL_PHONEMES):
-        for phoneme_2 in ALL_PHONEMES[i + 1:]:
-            type_: str
-            pair: tuple(str, str)
-            if phoneme_1 in CONSONANTS and phoneme_2 in VOWELS:
-                type_ = "cv"
-                pair = phoneme_1, phoneme_2
-            elif phoneme_2 in CONSONANTS and phoneme_1 in VOWELS:
-                type_ = "cv"
-                pair = phoneme_2, phoneme_1
-            elif phoneme_1 in CONSONANTS and phoneme_2 in CONSONANTS:
-                type_ = "cc"
-                pair = phoneme_1, phoneme_2
-            elif phoneme_1 in VOWELS and phoneme_2 in VOWELS:
-                type_ = "vv"
-                pair = phoneme_1, phoneme_2
-            elif "_" in (phoneme_1, phoneme_2):
-                if phoneme_1 == "_":
-                    pair = phoneme_2, phoneme_1
-                else:
-                    pair = phoneme_1, phoneme_2
-                if phoneme_1 in VOWELS:
-                    type_ = "_v"
-                elif phoneme_1 in CONSONANTS:
-                    type_ = "_c"
-
-            if type_ == "cv":
-                word_list.append({
-                    "word": ["t", "A", pair[0], pair[1], pair[0], "A"],
-                    "segments": [
-                        [pair[0], pair[1]],
-                        [pair[1], pair[0]],
-                    ]
-                })
-            elif type_ == "cc" and pair[0] != "h":
-                word_list.append({
-                    "word": ["t", "A", pair[0], pair[1], "A"],
-                    "segments": [[pair[0], pair[1]]],
-                })
-                word_list.append({
-                    "word": ["t", "A", pair[1], pair[0], "A"],
-                    "segments": [[pair[1], pair[0]]],
-                })
-            elif type_ == "vv":
-                word_list.append({
-                    "word": ["t", pair[0], pair[1]],
-                    "segments": [[pair[0], pair[1]]],
-                })
-                word_list.append({
-                    "word": ["t", pair[1], pair[0]],
-                    "segments": [[pair[1], pair[0]]],
-                })
-            elif type_ == "_v":
-                word_list.append({
-                    "word": [pair[0], "t", "A", "t", pair[0]],
-                    "segments": [
-                        ["_", pair[0]],
-                        [pair[0], "_"],
-                    ],
-                })
-                word_list.append({
-                    "word": [pair[0]],
-                    "segments": [[pair[0]]],
-                })
-            elif type_ == "_c":
-                word_list.append({
-                    "word": [pair[0], "A", pair[0]],
-                    "segments": [
-                        ["_", pair[0]],
-                        [pair[0], "_"],
-                    ],
-                })
-
-    return word_list
-
 
 def generate_base():
     for phoneme_1 in sorted(ALL_PHONEMES):
         for phoneme_2 in sorted(ALL_PHONEMES):
             if phoneme_1 != phoneme_2:
                 print(phoneme_1 + phoneme_2)
+
 
 def parse_wordlist(f):
     orphaned_diphones = []
@@ -216,18 +140,18 @@ def parse_wordlist(f):
         if len(parts) == 1:
             orphaned_diphones.extend(diphones)
         elif len(parts) == 3:
-            pronunciation = line[1]
-            text = line[2]
+            pronunciation = parse_pronunciation(parts[1][1:-1])
+            pronunciation = normalize_pronunciation(pronunciation)
+            text = parts[2]
+            for diphone in diphones:
+                if not is_sublist(diphone, pronunciation):
+                    raise RuntimeError(f"{diphone} is not in {pronunciation}")
         else:
             raise RuntimeError(f"Parse error: {line}")
+
 
 if __name__ == "__main__":
 
     with open("words.txt") as f:
         parse_wordlist(f)
 
-    #word_list = generate_nonsense_word_list()
-    #random.shuffle(word_list)
-    #for word in word_list:
-    #    print(as_ipa_string(word["word"]))
-    #print(len(word_list), "words")
