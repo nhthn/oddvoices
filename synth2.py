9f273633679cea0fffbe46ea995681537018a4c8
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-02-17 21:25:08 -0800

    Add (buggy) crossfading

--- synth2.py
+++ synth2.py
@@ -5,16 +5,20 @@ import phonology
 
 class Grain:
 
-    def __init__(self, frame, frame_length):
+    def __init__(self, frame, old_frame, frame_length, crossfade):
         self.frame = frame
+        self.old_frame = old_frame
         self.frame_length = frame_length
+        self.crossfade = crossfade
         self.playing = True
         self.read_pos = 0
 
     def process(self):
         if not self.playing:
             return 0
-        result = self.frame[self.read_pos] / 32767
+        result = self.frame[self.read_pos] / 32767 * (1 - self.crossfade)
+        if self.crossfade != 0:
+            result += self.old_frame[self.read_pos] / 32767 * self.crossfade
         self.read_pos += 1
         if self.read_pos == self.frame_length:
             self.playing = False
@@ -35,7 +39,17 @@ class Synth:
 
         self.grains = []
 
+        self.crossfade_length = 0.03
+
+        self.segment_id = None
+        self.segment_time = 0.0
+        self.old_segment_id = None
+        self.old_segment_time = 0.0
+        self.crossfade = 0
+        self.crossfade_ramp = 0
+
         self.segment_queue = []
+        self.vowel = False
         self.new_syllable()
         self.new_segment()
 
@@ -45,9 +59,19 @@ class Synth:
 
         frame_index = int(self.segment_time * self.database["expected_f0"])
         frame_index = frame_index % self.database[self.segment_id].shape[0]
+
+        if self.old_segment_id is not None:
+            old_frame_index = int(self.old_segment_time * self.database["expected_f0"])
+            old_frame_index = old_frame_index % self.database[self.old_segment_id].shape[0]
+            old_frame = self.database[self.old_segment_id][old_frame_index, :]
+        else:
+            old_frame = None
+
         grain = Grain(
             self.database[self.segment_id][frame_index, :],
-            self.frame_length
+            old_frame,
+            self.frame_length,
+            crossfade=self.crossfade
         )
         self.grains.append(grain)
 
@@ -63,6 +87,16 @@ class Synth:
         if self.segment_queue[0] == "|":
             self.segment_queue.pop(0)
             self.new_syllable()
+
+        self.old_segment_id = self.segment_id
+        self.old_segment_time = self.segment_time
+        if self.old_segment_id is None:
+            self.crossfade = 0
+            self.crossfade_ramp = 0
+        else:
+            self.crossfade = 1
+            self.crossfade_ramp = -1 / (self.crossfade_length * self.rate)
+
         self.segment_id = self.segment_queue.pop(0)
         self.segment_length = self.database[self.segment_id].shape[0] / self.database["expected_f0"]
         self.vowel = self.segment_id in phonology.VOWELS
@@ -76,8 +110,11 @@ class Synth:
             self.start_grain()
             self.phase -= 1
 
+        self.crossfade = max(self.crossfade + self.crossfade_ramp, 0.0)
+
+        self.old_segment_time += 1 / self.rate
         self.segment_time += 1 / self.rate
-        if self.segment_time >= self.segment_length and not self.vowel:
+        if self.segment_time >= self.segment_length - self.crossfade_length and not self.vowel:
             self.new_segment()
 
         return result
@@ -99,11 +136,11 @@ if __name__ == "__main__":
     result = []
     for i in range(int(1.0 * synth.rate)):
         result.append(synth.process())
-    synth.frequency *= 1.5
+    synth.frequency *= 9/8
     synth.new_segment()
     for i in range(int(1.0 * synth.rate)):
         result.append(synth.process())
-    synth.frequency *= 1.5
+    synth.frequency *= 9/8
     synth.new_segment()
     for i in range(int(1.0 * synth.rate)):
         result.append(synth.process())
