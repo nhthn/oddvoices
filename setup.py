ab9b43960b12602bd069bc1779f4ff094f2ead16
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-03-08 11:05:19 -0800

    Add mido to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     python_requires=">=3.8",
-    install_requires=["numpy", "scipy", "soundfile"],
+    install_requires=["numpy", "scipy", "soundfile", "mido"],
     entry_points={
         "console_scripts": [
             "sing = oddvoices.frontend:main",
